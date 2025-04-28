e4de3ec63a0e979a168105a1e41ea9a62023435a
ahnjinwon <99155067+ahnjinwon@users.noreply.github.com>
2025-04-28 17:25:34 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,11 +1,16 @@
 class Solution {
     public long solution(int n) {
-        int[] arr = new int[n+2];
-        arr[1]=1;
-        arr[2]=2;
-        for(int i=3; i<=n; i++){
+        if(n==1){
+            return 1;
+        }else if(n==2){
+            return 2;
+        }
+        int[] arr = new int[n];
+        arr[0]=1;
+        arr[1]=2;
+        for(int i=2; i<n; i++){
             arr[i]=(arr[i-2]+arr[i-1])%1234567;
         }
-        return arr[n];
+        return arr[n-1];
     }
 }
