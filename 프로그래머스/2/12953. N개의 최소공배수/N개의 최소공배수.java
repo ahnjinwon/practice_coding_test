a8b5f08b1fa5b02127e64f5d9be282dce66f3805
ahnjinwon <99155067+ahnjinwon@users.noreply.github.com>
2025-04-29 17:36:21 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.52 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        Arrays.sort(arr);
+        int answer = lcm(arr[0], arr[1]);
+        for(int i=2; i<arr.length; i++){
+            answer=lcm(answer,arr[i]);
+        }
+        return answer;
+    }
+    
+    private int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+    
+    private int lcm(int a, int b){
+        return (a*b)/gcd(a,b);
+    }
+}
