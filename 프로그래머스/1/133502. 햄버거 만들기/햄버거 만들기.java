0dd0b3d8da7bb2843eccf8ea4dc240bd0a6e8eee
ahnjinwon <99155067+ahnjinwon@users.noreply.github.com>
2025-05-14 16:45:07 +0900

    Update 햄버거 만들기.java

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,29 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        List<Integer> arr = new ArrayList<>();
-
-        for (int i : ingredient) {
-            arr.add(i);
-
-            int size = arr.size();
-            if (size >= 4 &&
-                arr.get(size - 4) == 1 &&
-                arr.get(size - 3) == 2 &&
-                arr.get(size - 2) == 3 &&
-                arr.get(size - 1) == 1) {
-                
-                arr.remove(size - 1);
-                arr.remove(size - 2);
-                arr.remove(size - 3);
-                arr.remove(size - 4);
-                
-                answer++;
+        Stack<Integer> stack = new Stack<>();
+        for(int i : ingredient){
+            stack.push(i);
+            
+            if(stack.size()>=4){
+                int size = stack.size();
+                if(stack.get(size-4)==1 && stack.get(size-3)==2 && stack.get(size-2)==3 && stack.get(size-1)==1){
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    answer++;
+                }
             }
         }
-
         return answer;
     }
 }
