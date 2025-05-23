7f6145d39ff768c465bf0fc12804cdb6f3d37228
ahnjinwon <99155067+ahnjinwon@users.noreply.github.com>
2025-05-23 16:54:09 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.35 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int term_cnt = 0;
+        int today_cnt = 0;
+        int privacies_cnt = 0;
+        for(int i=0; i<privacies.length; i++){
+            String[] privacy = privacies[i].split(" ");
+            String[] p_date = privacy[0].split("\\.");
+            String[] t_date = today.split("\\.");
+            
+            privacies_cnt = Integer.parseInt(p_date[0]) * 12 * 28
+                            + Integer.parseInt(p_date[1]) * 28
+                            + Integer.parseInt(p_date[2]);
+            
+            today_cnt = Integer.parseInt(t_date[0]) * 12 * 28
+                          + Integer.parseInt(t_date[1]) * 28
+                          + Integer.parseInt(t_date[2]);
+            
+            term_cnt = today_cnt - privacies_cnt;
+            
+            for(int j=0; j<terms.length; j++){
+                String[] term = terms[j].split(" ");
+                if(term[0].equals(privacy[1]) && Integer.parseInt(term[1])*28<=term_cnt){
+                    answer.add(i+1);
+                }
+            }
+        }
+        int[] ans = new int[answer.size()];
+        for(int i=0; i<answer.size(); i++){
+            ans[i] = answer.get(i);
+        }
+        return ans;
+    }
+}
